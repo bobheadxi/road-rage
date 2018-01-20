d07571093f6daff3e75449a82f858d26c9a4615d
Brian Nguyen <briannguyen992@gmail.com>
2018-01-20 12:37:07 -0800

    inputs for coordinates

--- web/src/App.js
+++ web/src/App.js
@@ -3,16 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      lat: 49.2827,
+      long: 123.1207
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  handleChange(e, l) {
+    if (l === 'lat') {
+      this.setState({ lat: e.target.value });
+    } else if (l === 'long') {
+      this.setState({ long: e.target.value });
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Road Rage</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, enter a set of coordinates.
         </p>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" name="lat" placeholder="enter latitude" value={this.state.lat} onChange={(e) => this.handleChange(e, 'lat')} />
+          <input type="text" name="long" placeholder="enter longitude" value={this.state.long} onChange={(e) => this.handleChange(e, 'long')} />
+          <input type="submit" name="submit" onClick={this.handleChange} />
+        </form>
       </div>
     );
   }
