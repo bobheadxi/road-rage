d51465ad2a85b4a977b3a46ad0f320bbcf97b5b3
Robert Lin <robertlin1@gmail.com>
2018-01-20 13:30:19 -0800

    POC server endpoint

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bobheadxi/road-rage/server/roadrage"
 	"github.com/bobheadxi/road-rage/server/tomtom"
 )
 
@@ -16,10 +17,13 @@ Longitude‎: ‎-123.116226
 
 func main() {
 	api := tomtom.New()
-	seg, err := api.GetSegmentAtCoordinate(49.246292, -123.116226)
+	seg, err := api.GetSegmentAtCoordinate("49.246292", "-123.116226")
 	if err != nil {
 		log.Fatal("Fuck" + err.Error())
 	}
 	segUnmarshall, _ := json.Marshal(seg)
 	fmt.Println(string(segUnmarshall))
+
+	server := roadrage.New(api)
+	server.Run("8000")
 }
--- server/roadrage/api.go
+++ server/roadrage/api.go
@@ -1,1 +1,51 @@
 package roadrage
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/bobheadxi/road-rage/server/tomtom"
+)
+
+type Server struct {
+	api *tomtom.API
+}
+
+func New(api *tomtom.API) *Server {
+	return &Server{
+		api: api,
+	}
+}
+
+func (s *Server) Run(port string) {
+	log.Print("Listening...")
+	http.HandleFunc("/build_game", s.buildGame)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func (s *Server) buildGame(w http.ResponseWriter, req *http.Request) {
+	lats, ok := req.URL.Query()["lat"]
+	if !ok || len(lats) < 1 {
+		http.Error(w, "Need latitude", http.StatusBadRequest)
+		return
+	}
+	lat := lats[0]
+	lons, ok := req.URL.Query()["lon"]
+	if !ok || len(lons) < 1 {
+		http.Error(w, "Need longitude", http.StatusBadRequest)
+		return
+	}
+	lon := lons[0]
+
+	seg, err := s.api.GetSegmentAtCoordinate(lat, lon)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	segUnmarshall, _ := json.Marshal(seg)
+	w.WriteHeader(http.StatusOK)
+	w.Write(segUnmarshall)
+}
